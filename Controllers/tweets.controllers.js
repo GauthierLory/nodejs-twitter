c306e1474948147ef80e1548fbc40369101bb7c5
GauthierLory <gauthier.lory@protonmail.com>
2021-02-16 11:46:34 +0100

    async method on controller

--- Controllers/tweets.controllers.js
+++ Controllers/tweets.controllers.js
@@ -1,20 +1,26 @@
-exports.tweetList = (req, res, next) => {
-    Tweet.find({})
-    .exec()
-    .then( tweets => res.render('tweets/tweet-list', { tweets }))
+const Tweet = require('../database/models/tweet.model');
+
+exports.tweetList = async (req, res, next) => {
+    try{
+        const tweets = await Tweet.find({}).exec();
+        res.render('tweets/tweet-list', { tweets });
+    }catch(e){
+        next(e);
+    }
 }
 
 exports.tweetNew = (req, res, next) => {
     res.render('tweets/tweet-form')
 }
 
-exports.tweetCreate = (req, res, next) => {
-    const body = req.body;
-    const newTweet = new Tweet(body);
-    newTweet.save()
-            .then( newTweet => res.redirect('/'))
-            .catch(err => {
-                const errors = Object.keys(err.errors).map(key => err.errors[key].message);
-                res.status(400).render('tweets/tweet-form', { errors} );
-            })
+exports.tweetCreate = async (req, res, next) => {
+    try{
+        const body = req.body;
+        const newTweet = new Tweet(body);
+        await newTweet.save()
+        res.redirect('/')
+    }catch(e){
+        const errors = Object.keys(e.errors).map(key => e.errors[key].message);
+        res.status(400).render('tweets/tweet-form', { errors} );
+    }  
 }
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const Tweet = require('../database/models/tweet.model');
-const { tweetList, tweetNew, tweetCreated } = require('../controllers/tweets.controllers');
+const { tweetList, tweetNew, tweetCreate } = require('../controllers/tweets.controllers');
 
 router.get('/', tweetList);
 router.get('/tweet/new', tweetNew);
