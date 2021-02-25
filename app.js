1df692301d3cae59cd8f179b9c2dace9735d0aac
GauthierLory <gauthier.lory@protonmail.com>
2021-02-25 16:27:20 +0100

    model & controller + package

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const path = require('path');
 const index = require('./routes');
 const errorHandler = require('errorhandler');
 require('./database');
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
+exports.app = app;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+require('./config/session.config');
 
 app.use(morgan('short'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,7 +26,7 @@ if (process.env.NODE_ENV === 'development'){
 } else {
     app.use((err, req, res, next) => {
         const code = err.code || 500
-        res.status(code).json({ 
+        res.status(code).json({
             code: code,
             message: code === 500 ? null : err.message
         })
