daf9d36801c5a36f700aa7fbec58eb34e418f49f
GauthierLory <gauthier.lory@protonmail.com>
2021-02-25 17:20:38 +0100

    inscription user

--- app.js
+++ app.js
@@ -21,16 +21,5 @@ app.use(index);
 
 console.log(process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === 'development'){
-    app.use(errorHandler());
-} else {
-    app.use((err, req, res, next) => {
-        const code = err.code || 500
-        res.status(code).json({
-            code: code,
-            message: code === 500 ? null : err.message
-        })
-    })
-}
 
 app.listen(port);
