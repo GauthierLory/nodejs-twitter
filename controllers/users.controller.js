9883bbfcf175b17d8c898326a7b9bb5e8b433ad0
Glory <gauthier.lory@protonmail.com>
2021-03-14 17:31:02 +0100

    add picture user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,14 @@
 const { createUser } = require('../queries/users.queries');
-
+const path = require('path');
+const multer = require('multer');
+const upload = multer({ storage: multer.diskStorage({
+    destination:(req, file, cb) => {
+      cb(null, path.join(__dirname,'../public/images/avatars'))
+    },
+    file: (req, file, cb) => {
+      cb(null, `${ Date.now() }-${ file.originalName }`);
+    }
+  })})
 exports.signupForm = (req, res, next) => {
   res.render('users/user-form', { errors: null, isAuthenticated: req.isAuthenticated(), currentUser: req.user});
 }
@@ -13,3 +22,17 @@ exports.signup = async (req, res, next) => {
     res.render('users/user-form', { errors: [ e.message ], isAuthenticated: req.isAuthenticated(), currentUser: req.user });
   }
 }
+
+exports.uploadImage = [
+    upload.single('avatar'),
+    async (req, res, next) => {
+      try{
+        const user = req.user;
+        user.avatar = `/images/avatars/${req.file.filename}`;
+        await user.save();
+        res.redirect('/');
+      }catch(e){
+        next(e);
+      }
+      res.end()
+}]
